8e8839925a65eb34a83bef38c6cc207303a34cf1
Daizaikun <leo-im@hotmail.com>
2023-01-12 05:37:10 +0000

    Prueba swr

--- src/components/BibleBook.jsx
+++ src/components/BibleBook.jsx
@@ -1,29 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useSWR from "swr";
 
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
+export default function BibleBook({ bookName }) {
+  const bibleApi = "https://bible-api.deno.dev";
 
-export default function BibleBook({bookName}){
-  const bibleApi = 'https://bible-api.deno.dev'
+  const { data, error, isLoading } = useSWR(
+    `${bibleApi}/book/${bookName.toLowerCase()}`,
+    fetcher
+  );
 
-  const [loadingChapter, setloadingChapter] = useState(true)
-  const [book, setBook] = useState([])
+  if (error) {
+    return <div> error</div>;
+  }
 
-  useEffect(() => {
-    fetch(`${bibleApi}/book/${bookName.toLowerCase()}`).then((d) => d.json()).then((book) => {
-      setBook(book)
-    }).catch((err) => {
-      throw err
-    })
-  })
-
-  
+  if (isLoading) {
+    return <div> is loading....</div>;
+  }
 
   return (
-    <h1>
-      book
-    </h1>
-  )
-
+    <div>
+      {data.map((datum) => (
+        <div key={datum?.chapter}>
+          <h1>Capítulo: {datum?.chapter}</h1>
+          <div>
+            {datum.vers.map((dat) => (
+              <div key={dat.number}>
+                <h3>Versículo: {dat.number}</h3>
+                <p>{dat.verse}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-
